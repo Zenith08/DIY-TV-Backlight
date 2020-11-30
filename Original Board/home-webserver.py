09720b5b30a12587168496a8b87a2540f4464b32
kricha02 <t5z2b@ugrad.cs.ubc.ca>
2020-11-29 17:44:48 -0800

    Enabled https.

--- Original Board/home-webserver.py
+++ Original Board/home-webserver.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
     comms.pins_only(["blue"])
     time.sleep(1)
     comms.pins_only([])
-    app.run(debug=False, port=8080, host='0.0.0.0')
+    app.run(debug=False, port=8080, host='0.0.0.0', ssl_context='adhoc')
     print ("cleanup")
     tasks.stop_task()
     comms.cleanup()
