30a794af29c2a72b532ef456e13a9d00c1019657
Zenith <kaidenr@shaw.ca>
2021-01-08 21:41:24 -0800

    Cleaned up intro script and added end video script.

--- Original Board/video-intro.py
+++ Original Board/video-intro.py
@@ -2,6 +2,18 @@ import comms
 import time
 
 if __name__ == '__main__':
+    time.sleep(1)
+    print("4")
+    time.sleep(1)
+    print("3")
+    time.sleep(1)
+    print("2")
+    time.sleep(1)
+    print("1")
+    time.sleep(0.5)
+    print("0.5")
+    time.sleep(0.5)
+    print("0")
     comms.pins_only(["green"]) #Opening Forest is green
     time.sleep(4.36)
     comms.pins_only(["green", "blue"]) # Turqoise Ocean 4:22
@@ -13,7 +25,8 @@ if __name__ == '__main__':
     comms.pins_only(["red", "blue"]) # Purple Movent 20:45
     time.sleep(3.25) # 3:15
     comms.pins_only(["blue"]) # Blue Spaceship 24:00
-    time.sleep(7.17) # 7:10
+    time.sleep(5.6) # 7:10
     comms.pins_only(["wwhite"]) # White Sun 27:55
-    time.sleep(2.75) # 2:45
+    time.sleep(1.75) # 2:45
     comms.pins_only([]) # End 30:50
+    comms.cleanup()
